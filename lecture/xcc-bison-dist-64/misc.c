c6436ff8d80c9bac6b806e72fc8c84811f1beae1
kazuki <kaz.tokyo.tech20@gmail.com>
2022-08-06 21:33:01 +0900

    formatによりcompile errorが生成されるのでrevert処理

--- lecture/xcc-bison-dist-64/misc.c
+++ lecture/xcc-bison-dist-64/misc.c
@@ -1,23 +1,25 @@
 /* XCC: Experimental C-subset compiler.
   Copyright (c) 2002-2016, gondow@cs.titech.ac.jp, All rights reserved.
-  $Id: misc.c,v 1.1 2016/05/19 04:10:05 gondow Exp gondow $ */
+  $Id: misc.c,v 1.1 2016/05/19 04:10:05 gondow Exp gondow $ */ 
 /* --------------------------------------------------------------------- */
-#include "misc.h"
-
-#include <assert.h> /* for assert */
 #include <stdio.h>
-#include <stdlib.h> /* for malloc */
-#include <string.h> /* for strcpy */
+#include <string.h>  /* for strcpy */
+#include <assert.h>  /* for assert */
+#include <stdlib.h>  /* for malloc */
+#include "misc.h"
 /* --------------------------------------------------------------------- */
 /* --------------------------------------------------------------------- */
 void *
-emalloc(size_t nbytes) {
-    void *p = malloc(nbytes);
-    if (p == NULL) perror("malloc");
+emalloc (size_t nbytes)
+{ 
+    void *p = malloc (nbytes);
+    if (p == NULL) perror ("malloc");
     return p;
 }
 
-void efree(void *ptr) {
-    free(ptr);
+void
+efree (void *ptr)
+{
+    free (ptr);
 }
 /* ---------------------------------------------------------------------- */
