767dff7fa0ce44058a09acccd0a7e7c79cece4a0
kazuki <kaz.tokyo.tech20@gmail.com>
2022-08-06 21:49:11 +0900

    errorが出ないようにformatを作用させた

--- lecture/xcc-bison-dist-64/misc.c
+++ lecture/xcc-bison-dist-64/misc.c
@@ -1,6 +1,6 @@
 /* XCC: Experimental C-subset compiler.
   Copyright (c) 2002-2016, gondow@cs.titech.ac.jp, All rights reserved.
-  $Id: misc.c,v 1.1 2016/05/19 04:10:05 gondow Exp gondow $ */ 
+  $Id: misc.c,v 1.1 2016/05/19 04:10:05 gondow Exp gondow $ */
 /* --------------------------------------------------------------------- */
 #include <stdio.h>
 #include <string.h>  /* for strcpy */
@@ -8,18 +8,14 @@
 #include <stdlib.h>  /* for malloc */
 #include "misc.h"
 /* --------------------------------------------------------------------- */
-/* --------------------------------------------------------------------- */
 void *
-emalloc (size_t nbytes)
-{ 
-    void *p = malloc (nbytes);
-    if (p == NULL) perror ("malloc");
+emalloc(size_t nbytes) {
+    void *p = malloc(nbytes);
+    if (p == NULL) perror("malloc");
     return p;
 }
 
-void
-efree (void *ptr)
-{
-    free (ptr);
+void efree(void *ptr) {
+    free(ptr);
 }
 /* ---------------------------------------------------------------------- */
