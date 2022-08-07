6b46b5aa1f4ad9afdd74b63cffdeef04b01d09aa
kazuki <kaz.tokyo.tech20@gmail.com>
2022-08-08 01:11:53 +0900

    format

--- lecture/xcc-bison-dist-64/test/kadai2.c
+++ lecture/xcc-bison-dist-64/test/kadai2.c
@@ -1,20 +1,18 @@
-int printf ();
+int printf();
 
-long mrn (long n)
-{
+long mrn(long n) {
     if (n < 0 || n == 0)
         return 0;
     else
-        return 10 * mrn (n - 1) + n;
+        return 10 * mrn(n - 1) + n;
 }
 
-int main ()
-{
-  long i;
+int main() {
+    long i;
 
-  i = 0;
-  while (i < 11) {
-    printf ("mrn(%ld) = %ld\n", i, mrn(i));
-    i = i+1;
-  }
+    i = 0;
+    while (i < 11) {
+        printf("mrn(%ld) = %ld\n", i, mrn(i));
+        i = i + 1;
+    }
 }
